2d038ed36e88c76c3dbedeb09cac860abfa95515
liliannss <708090git>
2021-12-21 20:35:43 -0300

    Update application.properties and SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (21, 'Arvo', '2021-07-05', '265463373', '67627802024', '324-763-1348', '251-269-0832', 'arvo@pen.io', '4268', 'Bashford', 'Bashford', 'Hayes');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(21, 'Arvo', '2021-07-05', '265463373', '67627802024', '324-763-1348', '251-269-0832', 'arvo@pen.io', '4268', 'Bashford', 'Bashford', 'Hayes');
--- src/main/resources/db/migration/V02__insert_into_table_usuario.sql
+++ src/main/resources/db/migration/V02__insert_into_table_usuario.sql
@@ -1,59 +1,59 @@
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (1, 'Job', '2021-07-05', '154582219', '97699769031', '324-763-1348', '251-269-0832', 'bridout0@pen.io', '4268', 'Bashford', 'Bashford', 'Hayes');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(1, 'Job', '2021-07-05', '154582219', '97699769031', '324-763-1348', '251-269-0832', 'bridout0@pen.io', '4268', 'Bashford', 'Bashford', 'Hayes');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (2, 'Voltsillam', '2021-12-16', '120195537', '48432096008', '733-768-0020', '987-456-2755', 'ematuska1@omniture.com', '4637', 'Loftsgordon', 'Springs', 'Corscot');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(2, 'Voltsillam', '2021-12-16', '120195537', '48432096008', '733-768-0020', '987-456-2755', 'ematuska1@omniture.com', '4637', 'Loftsgordon', 'Springs', 'Corscot');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (3, 'Redhold', '2021-04-08', '182383106', '35289384049', '103-300-7597', '949-886-8033', 'acreese2@redcross.org', '945', 'Namekagon', 'Corben', 'Chive');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(3, 'Redhold', '2021-04-08', '182383106', '35289384049', '103-300-7597', '949-886-8033', 'acreese2@redcross.org', '945', 'Namekagon', 'Corben', 'Chive');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (4, 'It', '2021-03-08', '431762405', '76397356027', '126-346-3483', '468-733-8211', 'etrathen3@youku.com', '95225', 'Mitchell', 'Walton', 'Scott');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(4, 'It', '2021-03-08', '431762405', '76397356027', '126-346-3483', '468-733-8211', 'etrathen3@youku.com', '95225', 'Mitchell', 'Walton', 'Scott');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (5, 'Tempsoft', '2021-07-05', '212734222', '76726953046', '562-902-6498', '184-399-0905', 'tbartoloma4@youtube.com', '075', 'Declaration', 'Messerschmidt', 'Riverside');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(5, 'Tempsoft', '2021-07-05', '212734222', '76726953046', '562-902-6498', '184-399-0905', 'tbartoloma4@youtube.com', '075', 'Declaration', 'Messerschmidt', 'Riverside');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (6, 'Mat Lam Tam', '2021-05-23', '370387429', '80889410089', '373-982-9859', '931-514-1130', 'edelyth5@mac.com', '00058', 'Summerview', 'Green', 'Londonderry');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(6, 'Mat Lam Tam', '2021-05-23', '370387429', '80889410089', '373-982-9859', '931-514-1130', 'edelyth5@mac.com', '00058', 'Summerview', 'Green', 'Londonderry');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (7, 'Keylex', '2021-10-23', '386752953', '23348272076', '607-304-5943', '374-480-9153', 'iadan6@ucoz.ru', '2502', 'Meadow Ridge', 'David', 'Nancy');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(7, 'Keylex', '2021-10-23', '386752953', '23348272076', '607-304-5943', '374-480-9153', 'iadan6@ucoz.ru', '2502', 'Meadow Ridge', 'David', 'Nancy');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (8, 'Fix San', '2021-01-13', '371235698', '17998760044', '641-892-5852', '298-416-3513', 'ataplow7@cornell.edu', '454', 'Westport', 'Elmside', 'Granby');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(8, 'Fix San', '2021-01-13', '371235698', '17998760044', '641-892-5852', '298-416-3513', 'ataplow7@cornell.edu', '454', 'Westport', 'Elmside', 'Granby');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (9, 'Viva', '2021-02-16', '295785135', '81532356056', '144-794-6337', '214-141-7115', 'bsnape8@nsw.gov.au', '9', 'Helena', 'Loeprich', 'Debs');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(9, 'Viva', '2021-02-16', '295785135', '81532356056', '144-794-6337', '214-141-7115', 'bsnape8@nsw.gov.au', '9', 'Helena', 'Loeprich', 'Debs');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (10, 'Gembucket', '2021-06-25', '108405655', '97133976064', '562-609-5950', '838-564-9226', 'mfisk9@e-recht24.de', '0', 'Ridgeview', 'Melody', 'Raven');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(10, 'Gembucket', '2021-06-25', '108405655', '97133976064', '562-609-5950', '838-564-9226', 'mfisk9@e-recht24.de', '0', 'Ridgeview', 'Melody', 'Raven');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (11, 'Otcom', '2021-10-08', '204920176', '00045358044', '579-827-7789', '369-985-5379', 'psjollemaa@amazon.co.uk', '774', 'Judy', 'North', 'Sutteridge');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(11, 'Otcom', '2021-10-08', '204920176', '00045358044', '579-827-7789', '369-985-5379', 'psjollemaa@amazon.co.uk', '774', 'Judy', 'North', 'Sutteridge');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (12, 'Keylex', '2021-12-05', '219379245', '25573079042', '650-768-6133', '297-358-6797', 'zhannayb@sbwire.com', '03', 'Granby', 'Knutson', 'New Castle');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(12, 'Keylex', '2021-12-05', '219379245', '25573079042', '650-768-6133', '297-358-6797', 'zhannayb@sbwire.com', '03', 'Granby', 'Knutson', 'New Castle');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (13, 'Cardify', '2021-06-24', '280199533', '60856521000', '377-821-7127', '277-454-8385', 'mraviliousc@columbia.edu', '52662', 'Basil', 'Grover', 'Kinsman');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(13, 'Cardify', '2021-06-24', '280199533', '60856521000', '377-821-7127', '277-454-8385', 'mraviliousc@columbia.edu', '52662', 'Basil', 'Grover', 'Kinsman');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (14, 'Matsoft', '2021-08-01', '490797544', '23773489048', '614-285-8403', '739-377-2560', 'cmcdarmidd@ftc.gov', '07', 'Truax', 'Banding', 'Del Mar');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(14, 'Matsoft', '2021-08-01', '490797544', '23773489048', '614-285-8403', '739-377-2560', 'cmcdarmidd@ftc.gov', '07', 'Truax', 'Banding', 'Del Mar');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (15, 'Job', '2021-03-11', '354827856', '27118491047', '108-837-7522', '934-269-2213', 'bantonine@mysql.com', '3672', 'Clemons', 'Kropf', 'Mosinee');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(15, 'Job', '2021-03-11', '354827856', '27118491047', '108-837-7522', '934-269-2213', 'bantonine@mysql.com', '3672', 'Clemons', 'Kropf', 'Mosinee');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (16, 'Alphazap', '2021-08-21', '123695594', '49737960009', '875-200-6331', '205-931-6812', 'egreswellf@thetimes.co.uk', '70', 'Mockingbird', 'Kensington', 'Scofield');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(16, 'Alphazap', '2021-08-21', '123695594', '49737960009', '875-200-6331', '205-931-6812', 'egreswellf@thetimes.co.uk', '70', 'Mockingbird', 'Kensington', 'Scofield');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (17, 'Voyatouch', '2021-02-28', '190665476', '77685295024', '358-499-0638', '400-569-3320', 'amivalg@naver.com', '8719', 'Sutteridge', 'Stuart', 'Cottonwood');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(17, 'Voyatouch', '2021-02-28', '190665476', '77685295024', '358-499-0638', '400-569-3320', 'amivalg@naver.com', '8719', 'Sutteridge', 'Stuart', 'Cottonwood');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (18, 'Zaam-Dox', '2021-05-11', '370498598', '91327475006', '788-480-9796', '745-531-6033', 'epartridgeh@bravesites.com', '24', 'Thierer', 'Northwestern', 'Swallow');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(18, 'Zaam-Dox', '2021-05-11', '370498598', '91327475006', '788-480-9796', '745-531-6033', 'epartridgeh@bravesites.com', '24', 'Thierer', 'Northwestern', 'Swallow');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (19, 'Flexidy', '2021-06-21', '262658847', '28663225091', '879-566-9517', '146-266-0487', 'jcrinioni@google.ru', '06246', 'Swallow', 'Laurel', 'Doe Crossing');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(19, 'Flexidy', '2021-06-21', '262658847', '28663225091', '879-566-9517', '146-266-0487', 'jcrinioni@google.ru', '06246', 'Swallow', 'Laurel', 'Doe Crossing');
 
-insert into usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
-values (20, 'Otcom', '2021-07-02', '147598862', '37766126060', '438-968-8757', '123-247-9513', 'kmacknessj@gnu.org', '8625', 'Fair Oaks', 'Schmedeman', 'Florence');
+INSERT INTO usuario (usuario_id, nome, nascimento, rg, cpf, telefone, celular, email, numero, logradouro, bairro, localidade)
+VALUES(20, 'Otcom', '2021-07-02', '147598862', '37766126060', '438-968-8757', '123-247-9513', 'kmacknessj@gnu.org', '8625', 'Fair Oaks', 'Schmedeman', 'Florence');
